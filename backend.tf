3e7ab5000334e6f8b6e702b53d22ef6fb736c1af
ahmedmabdi <ahmed.abdi67@outlook.com>
2025-09-05 08:06:43 +0100

    add backend

--- backend.tf
+++ backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket       = "ny-terraform-states"
-    key          = "umami/terraform.tfstate"
-    region       = "eu-west-2"
-    encrypt      = true
-    use_lockfile = true
+    bucket = "ny-terraform-states"
+    key = "umami/terraform.tfstate"
+    region = "eu-west-2"
+    dynamodb_table = "terraform-locks"
+    encrypt = true
   }
 }
