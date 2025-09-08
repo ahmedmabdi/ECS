2a318ea6aefc9bd5c6eecee243f13ff4bb22dcff
Ahmed Abdi <ahmed.abdi67@outlook.com>
2025-09-08 21:27:14 +0100

    Update variables.tf

--- terraform/modules/sg/variables.tf
+++ terraform/modules/sg/variables.tf
@@ -1,10 +1,8 @@
 variable "vpc_id" {
-  description = "The VPC ID where the SGs will be created"
   type        = string
 }
 
 variable "project_name" {
-  description = "Name prefix for security groups"
   type        = string
   default     = "ecs-umami"
 }
